d47a4b349071ec00cf46a10eed8021441f77af34
Today <76273733+today2098@users.noreply.github.com>
2025-07-03 02:21:53 +0900

    [add] Create two scripts to automatically verify test cases

---
+++ check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+oj t -c debug.out -d test/$1
---
+++ download_test_case.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+oj d "https://atcoder.jp/contests/$1/tasks/${1}_a" -d test/a
+oj d "https://atcoder.jp/contests/$1/tasks/${1}_b" -d test/b
+oj d "https://atcoder.jp/contests/$1/tasks/${1}_c" -d test/c
+oj d "https://atcoder.jp/contests/$1/tasks/${1}_d" -d test/d
+oj d "https://atcoder.jp/contests/$1/tasks/${1}_e" -d test/e
+oj d "https://atcoder.jp/contests/$1/tasks/${1}_f" -d test/f
